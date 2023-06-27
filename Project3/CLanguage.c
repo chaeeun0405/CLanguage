2bcd53b51aa0ea3826f85b3e07997668e37a56a2
chaeeun <angelasce@naver.com>
2023-06-27 19:44:36 +0900

    사칙연산

--- Project3/CLanguage.c
+++ Project3/CLanguage.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main() {
-	int N, i;
-	scanf("%d", &N);
-	for (i = 1; i <= 9; i++) {
-		printf("%d * %d = %d\n", N, i, N * i);
-	}
-
+	int A, B;
+	scanf("%d %d", &A, &B);
+	printf("%d\n", A + B);
+	printf("%d\n", A - B);
+	printf("%d\n", A * B);
+	printf("%d\n", A / B); //숫자가 둘 다 정수면 정수형 나눗셈이 실행돼서 소숫점 절삭되므로 형변환!!
+	printf("%d", A % B);
 	return 0;
 }
